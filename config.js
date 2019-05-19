5c4ac186474c9a36311655829a8cf6771f59841c
admin <admin@adon.network>
2019-05-19 18:43:08 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+var api = "http://pool.adon.network/api";
+
+var email = "pool-support@adon.network";
+var telegram = "https://t.me/adon_coin";
+var discord  = "https://discord.gg/UrTctGD";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer  = "https://blocks.adon.network?hash={symbol}/#block";
+var transactionExplorer = "https://blocks.adon.network/?hash={symbol}/#transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
